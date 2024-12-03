a09c330bacc1cbce781526358ed11af4f031f5e4
Tristan Rasmussen <tristanrasmussen@tristanrasmussen.com>
2024-12-03 15:12:30 +0000

    fix: use correct chakra ui components

--- src/components/pages/Results.tsx
+++ src/components/pages/Results.tsx
@@ -1,41 +1,41 @@
 import { solveProblem } from "@/logic/solve-problem";
 import { Answer } from "@/types/answer";
-import { VStack, Text, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { VStack, Text, Table } from "@chakra-ui/react";
 
 export const Results: React.FC<{ history: Answer[] }> = ({ history }) => {
   return (
     <VStack gap={4} align="start">
       <Text fontSize="2xl">History</Text>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Problem</Th>
-            <Th>Your Answer</Th>
-            <Th>Correct</Th>
-            <Th>Correct Answer</Th>
-            <Th>Time Taken (s)</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>Problem</Table.ColumnHeader>
+            <Table.ColumnHeader>Your Answer</Table.ColumnHeader>
+            <Table.ColumnHeader>Correct</Table.ColumnHeader>
+            <Table.ColumnHeader>Correct Answer</Table.ColumnHeader>
+            <Table.ColumnHeader>Time Taken (s)</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
           {history.map((answer, index) => {
             const correctAnswer = solveProblem(answer.problem);
             const isCorrect = answer.answer === correctAnswer;
             return (
-              <Tr key={index}>
-                <Td>
+              <Table.Row key={index}>
+                <Table.Cell>
                   {answer.problem.operand1}{" "}
                   {answer.problem.type === "addition" ? "+" : "*"}{" "}
                   {answer.problem.operand2}
-                </Td>
-                <Td>{answer.answer}</Td>
-                <Td>{isCorrect ? "Yes" : "No"}</Td>
-                <Td>{isCorrect ? "-" : correctAnswer}</Td>
-                <Td>{answer.timeTaken.toFixed(2)}</Td>
-              </Tr>
+                </Table.Cell>
+                <Table.Cell>{answer.answer}</Table.Cell>
+                <Table.Cell>{isCorrect ? "Yes" : "No"}</Table.Cell>
+                <Table.Cell>{isCorrect ? "-" : correctAnswer}</Table.Cell>
+                <Table.Cell>{answer.timeTaken.toFixed(2)}</Table.Cell>
+              </Table.Row>
             );
           })}
-        </Tbody>
-      </Table>
+        </Table.Body>
+      </Table.Root>
     </VStack>
   );
 };
